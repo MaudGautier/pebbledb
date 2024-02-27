ffbe1ad21783b51874cf2deee5db20781e422e0d
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-27 18:08:41 -0500

    test(RBTree): add test to insert on the left and on the right of a root

--- src/__tests__/test_red_black_tree.py
+++ src/__tests__/test_red_black_tree.py
@@ -1,7 +1,7 @@
 from src.red_black_tree import Node, extract_values_from_list_of_nodes, bfs
 
 
-def test_insert_one_node():
+def test_insert_one_node_on_the_right():
     # GIVEN
     root = Node(value=10)
 
@@ -13,3 +13,15 @@ def test_insert_one_node():
     expected_values = [10, None, 11, None, None]
     assert values == expected_values
 
+
+def test_insert_one_node_on_the_left():
+    # GIVEN
+    root = Node(value=10)
+
+    # WHEN
+    root.insert(Node(9))
+
+    # THEN
+    values = extract_values_from_list_of_nodes(bfs(root=root))
+    expected_values = [10, 9, None, None, None]
+    assert values == expected_values
