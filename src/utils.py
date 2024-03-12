56ec35130a8bf58789eb03d854fbaddf4a7ff7d4
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-12 00:05:33 -0400

    perf(iterators): run merge iterator in o(log n) instead of o(n) (use priority queue)

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from typing import Iterator, Any
+from heapq import heappush, heappop
 
 
 def merge_iterators(iterators: list[Iterator[Any]]) -> Iterator[Any]:
@@ -10,13 +11,15 @@ def merge_iterators(iterators: list[Iterator[Any]]) -> Iterator[Any]:
         except StopIteration:
             return has_no_value
 
-    next_values = []
+    heap = []
     for i, iterator in enumerate(iterators):
         next_value = get_next(iterator)
-        next_values.append((next_value, i))
+        heappush(heap, (next_value, i))
 
-    while len(remaining_values := [value for value in next_values if value[0] is not has_no_value]):
-        value_to_yield, idx = min(remaining_values)
-        yield value_to_yield
+    while len(heap):
+        value, idx = heappop(heap)
+        yield value
         next_value = get_next(iterators[idx])
-        next_values[idx] = (next_value, idx)
+        if next_value is has_no_value:
+            continue
+        heappush(heap, (next_value, idx))
