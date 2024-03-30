3ee00249e24cae8f186e43e43e31aab12ac00523
Maud Gautier <maud.j.gautier@gmail.com>
2024-03-29 22:27:59 -0400

    feat(bloom): implement bloom filters

---
+++ src/__tests__/test_bloom_filter.py
@@ -0,0 +1,20 @@
+from src.bloom_filter import BloomFilter
+
+
+def test_lookups():
+    # GIVEN
+    bloom_filter = BloomFilter(bits_size=32, nb_hash_functions=3)
+    keys = ["foo", "bar", "baz"]
+    missing_keys = ["not_in_bloom_filter", "missing"]
+
+    # WHEN
+    for key in keys:
+        bloom_filter.add(key=key)
+
+    # THEN
+    for key in keys:
+        assert bloom_filter.may_contain(key=key) is True
+
+    # They could be true, but in this case we know they return False (probabilistic)
+    for key in missing_keys:
+        assert bloom_filter.may_contain(key=key) is False
---
+++ src/bloom_filter.py
@@ -0,0 +1,78 @@
+import hashlib
+from typing import Callable
+
+
+class BloomFilter:
+    """This class implements a bloom filter.
+    A bloom filter is a space-efficient probabilistic data structure that is used to hint on the possible presence
+    (or guaranteed absence) of an item in a collection.
+
+    A bloom filter is a sequence of bits that are set or unset depending on the items present in the collection.
+    Every time we add a key to the collection, we hash it with a given hash function and then take the modulo of the
+    resulting hash by the size of the sequence of bits (so that hashed values are distributed over this range).
+    The bit corresponding to the hashed key is set.
+    The process is repeated for `n` hash functions. Thus, about `n` bits are set for every key included in the
+    collection (or possibly fewer if two hash functions turn the same bit on).
+
+    To know if a key is in the collection, we need to hash the key by all hash functions and check the status of all
+    those bits in the sequence:
+    - If they are all set, then the key may be in the collection;
+    - If at least one of them is not set, then we are guaranteed that the key is not in the collection.
+    """
+
+    def __init__(self, bits_size: int, nb_hash_functions: int):
+        self.bits_size = bits_size
+        self.hash_functions = self._select_hash_functions(nb_hash_functions)
+        self.bits = 1 << self.bits_size
+
+    @staticmethod
+    def _select_hash_functions(n: int) -> list[Callable]:
+        available_hash_functions = [
+            hashlib.sha224,
+            hashlib.sha256,
+            hashlib.sha384,
+            hashlib.sha512,
+            hashlib.blake2b,
+            hashlib.blake2s,
+        ]
+        assert n <= len(available_hash_functions), "Too many hash functions required."
+        return available_hash_functions[0:n]
+
+    def _hash(self, key: str) -> list[int]:
+        """Hashes the key with all hash functions and defines the list of bits that should be set.
+        After hashing, we take the modulo of the result by the size of the sequence of bits so that all hash functions
+        are mapped to the same output range.
+        """
+        encoded_key = key.encode(encoding="utf-8")
+        selected_bits = []
+        for hash_function in self.hash_functions:
+            hashed_key = hash_function(encoded_key)
+            i = int(hashed_key.hexdigest(), base=16)
+            selected_bit = i % self.bits_size
+            selected_bits.append(selected_bit)
+        return selected_bits
+
+    def _set_bit(self, bit_index: int) -> None:
+        assert bit_index <= self.bits_size, "Selected bit is bigger than the size of the bloom filter."
+        bit = (1 << bit_index)
+        self.bits |= bit
+
+    def _is_bit_set(self, bit_index: int) -> bool:
+        bit = (1 << bit_index)
+        return (self.bits & bit) == bit
+
+    def add(self, key: str) -> None:
+        """Adds a key to the bloom filter.
+        """
+        bits_to_set = self._hash(key=key)
+        for bit in bits_to_set:
+            self._set_bit(bit_index=bit)
+
+    def may_contain(self, key: str) -> bool:
+        """Returns True if the key may be in the bloom filter, False if it is guaranteed not to be in it.
+        """
+        bits_to_check = self._hash(key=key)
+        for bit in bits_to_check:
+            if not self._is_bit_set(bit_index=bit):
+                return False
+        return True
